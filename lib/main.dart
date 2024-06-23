0d18add3ec8b78dff9225c64f8e122bebc5522ab
ryusron19 <ryusron19@gmail.com>
2024-06-23 07:35:24 +0000

    widget column dan row

--- lib/main.dart
+++ lib/main.dart
@@ -10,20 +10,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('My Application')),
-        body: Center(
-            child: Container(
-                color: Colors.red,
-                width: 150,
-                height: 100,
-                child: const Text(
-                  'Hello Broh ini aplikasi pertama saya di flutter idx',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.bold, fontSize: 10),
-                ))),
-      ),
+          appBar: AppBar(
+            title: const Text('My First App'),
+          ),
+          body: const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('Column 1'),
+              Text('Column 2'),
+              Text('Column 3'),
+              Row(children: <Widget>[
+                Text('Row 1'),
+                Text('Row 2'),
+                Text('Row 3'),
+              ])
+            ],
+          )),
     );
   }
 }
